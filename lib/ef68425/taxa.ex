3270a10483780fa994ffe1318c946712afcde9ac
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2024-12-30 11:10:56 -0600

    add schema

---
+++ lib/ef68425/taxa.ex
@@ -0,0 +1,26 @@
+defmodule Backend.Taxas.Taxa do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Backend.Recintos.Recinto
+  alias Backend.Servicos.Servico
+  alias Backend.Taxas.Valores
+
+  @derive {Jason.Encoder, [except: [:__meta__, :recinto, :servico]]}
+  schema "taxas" do
+    belongs_to :recinto, Recinto
+    belongs_to :servico, Servico
+    field :tipo, Ecto.Enum, values: [:publica, :negociada]
+    embeds_one :valores, Valores
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(taxa, attrs) do
+    taxa
+    |> cast(attrs, [:servico_id, :recinto_id, :tipo])
+    |> validate_required([:servico_id, :recinto_id, :tipo])
+    |> cast_embed(:valores, [required: true])  # Fix here for embedding
+    |> foreign_key_constraint(:servico_id, message: "Servico not found")
+    |> foreign_key_constraint(:recinto_id, message: "Recinto not found")
+  end
+end
